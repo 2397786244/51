54f20f5c9dbd02e1a0e726522a256f5d219ca3cd
2397786244 <2397786244@qq.com>
2022-04-11 23:22:32 +0800

    新增温度显示功能，完成串口发送时间的功能

--- 简易时钟－日历＿DS1302/clock.c
+++ 简易时钟－日历＿DS1302/clock.c
@@ -1,15 +1,18 @@
 #include <reg52.h>
 #include <stdio.h>
+#include <intrins.h> //nop函数原型
 //DS1302写寄存器 起始地址
 #define REG_W_START 0x80
 //DS1302 读寄存器 起始地址
 #define REG_R_START 0x81
 
 /*
-使用LCD1602显示当前的时间
-按下按键S2，显示当前的年月日信息(星期）。
-使用ds1302 RAM储存信息。
-按下S3，发送时间到串口。
+功能：
+	使用LCD1602显示当前的时间
+	按下按键S2，显示当前的年月日信息(星期）。
+	使用ds1302 RAM储存信息。
+	按下S4，发送时间到串口。
+	温度显示
 */
 
 //DS1302三个引脚使用P1.0 1.1 1.2，用杜邦线提前连接
@@ -24,7 +27,10 @@ sbit RW = P3^6; //数据读写。
 
 //按键
 sbit S2 = P3^0;
-sbit S3 = P3^1;
+sbit S4 = P3^2;
+
+//DS18B20 数据传输口 1-wire
+sbit DIO = P2^2;
 
 typedef unsigned char uchar;
 typedef unsigned int uint;
@@ -34,8 +40,8 @@ uchar TIME_AR[7] = {
 20,50,5,14,7,1,22
 };
 
-//设置LCD显示状态
-enum STATES {CLOCK,Y_M_D};
+//设置LCD显示状态,分别为时间，日历，温度显示。
+enum STATES {CLOCK,Y_M_D,TEMPTURE};
 static enum STATES S = CLOCK;
 
 void sendTimeToSeries();
@@ -46,6 +52,13 @@ void delay(uchar time){
 		for(j = 0;j < time;j++);
 	}
 }
+
+//延时， 约6毫秒
+void delay_us(uchar time)
+{
+	while(time--);
+}
+
 //判断是否空闲
 void readStates(){
 	uchar flag = 0;
@@ -243,6 +256,101 @@ void timeDisplay_YMD(){
 	SendData(' ');
 }
 
+
+//温度传感器 DS18B20 部分
+//每次对器件操作的时候都要初始化。
+bit DS18B20Init()
+{
+	bit flag;
+	DIO = 1;
+	_nop_();
+	DIO = 0;
+	delay_us(80); //保持480us以上。
+	DIO = 1;
+	delay_us(10); //延时 15 -60us
+	flag = DIO;
+	delay_us(20); //读取器件发送的信号（低电平表示存在）之后，保持60-240us。
+	DIO = 1; //释放总线
+	return flag;	
+}
+
+//往器件发送一字节数据
+void DS18B20SendByte(uchar dat){
+	uint i;
+	for(i = 0;i<8;i++){
+		DIO = 1;
+		DIO = 0;
+		_nop_(); //产生写时序，将总线拉低至少1us。
+		DIO = dat & 0x01;
+		delay_us(15);
+		dat >>= 1; 
+		_nop_(); //两个写时序之间至少间隔1us。
+	}
+	DIO = 1;
+}
+
+//产生一个读时序。
+uchar DS18B20ReadByte(){
+	uint i,j;
+	uchar dat;
+	//产生读时序，将总线拉低
+	for(i = 0;i<8;i++){
+		DIO = 1;
+		DIO = 0;
+		_nop_(); //拉低总线至少1us
+		DIO = 1;
+		_nop_();//释放总线
+		j= DIO;
+		delay_us(10); //读时序保持60us以上。
+		dat = (j << 7) | (dat >> 1);
+		_nop_(); //两个读时序之间间隔1us。
+	}
+	DIO = 1;
+	return dat;
+}
+
+void temptureDisplay(){
+	uchar H_BYTE;
+	uchar L_BYTE;
+	uint TEMP;
+	//每次显示温度，先初始化，再发送跳过ROM指令（针对单器件的情况），再发送
+	//温度转换指令。第二次执行重复操作，发送读取温度寄存器的指令。
+	DS18B20Init();
+	DS18B20SendByte(0xcc); //忽略ROM
+	DS18B20SendByte(0x44); //温度转换，结果储存在Byte0-1 寄存器中。
+	DS18B20Init();
+	DS18B20SendByte(0xcc);
+	DS18B20SendByte(0xbe); // 温度读取
+	L_BYTE = DS18B20ReadByte();
+	H_BYTE = DS18B20ReadByte();
+	TEMP = H_BYTE;
+	TEMP <<= 8;
+	TEMP |= L_BYTE;
+	TEMP = TEMP * 0.0625 * 10 + 0.5; //四舍五入，
+	//温度显示保留一位小数。显示在LCD右下角。
+	setPointer(1,0);
+	SendString("TODAY ROOM TEMP");
+	setPointer(2,0);
+	//发送TEMP每一位上的数字
+	SendData(TEMP / 100 + '0');
+	SendData(TEMP / 10 % 10 + '0');
+	SendData('.');
+	SendData(TEMP % 10 + '0');
+	SendData(0xdf);
+	SendData('C');
+	SendData(' ');
+	if(TEMP > 280){
+		SendString("HOT!");
+	}
+	else if(TEMP > 120){
+		SendString("COOL!");
+	}
+	else if(TEMP > 10){
+		SendString("COLD!");
+	}
+	SendData(' ');
+}
+
 //按键部分。S2用于切换两种显示，S3记录时间。
 void statesAlter(){
 	if(!S2){
@@ -250,39 +358,49 @@ void statesAlter(){
 		if(!S2){
 			switch(S){
 				case CLOCK:S = Y_M_D;break;
-				case Y_M_D:S = CLOCK;break;
+				case Y_M_D:S = TEMPTURE;break;
+				case TEMPTURE: S = CLOCK;break;
 			}
 		
 		}
 		while(!S2); //松手检测
 	}
-	if(!S3){
+	if(!S4){
 		delay(10);
-		if(!S3)
+		if(!S4)
 		{
 			sendTimeToSeries();
-			//按下S3，把当前时间发送到串口。
-			while(!S3);
+			while(!S4);
 		}
 	}
+	
 }
 
 void timerInit(){
-	TR1 = 1; //定时器1工作。
-	TMOD |= 0x20;
+	EA = 1;
+	TMOD = 0x20;
 	TH1 = 0xfd;
 	TL1 = 0xfd; //波特率9600
 	SM0 = 0;
 	SM1 = 1; //串口选择工作模式
+	ET1 = 0;
+	TR1 = 1; //定时器1工作。
 }
 
 void sendTimeToSeries()
 {
-	/*
-	串口发送
-	*/
+	uint i;	
+	//按下S4(P3^2IO口)，把当前时间发送到串口。
+	//注意串口通信使用P3.0 3.1口，使用按键和UART不要在同一个IO口上。
+	for(i = 0;i<7;i++)
+	{
+		SBUF = TIME_AR[i];
+		while(!TI);
+		TI = 0;
+	}
 }
 
+//主函数部分，每次循环检测按键（显示内容切换），每隔一段时间在LCD上显示值（可以放到定时器内）
 void main()
 {
 	uint cnt = 0;
@@ -294,15 +412,17 @@ void main()
 		cnt++;
 		if(cnt >= 3000)
 		{
+			
 			writeOpen(0);
 			switch(S)
 			{
 				case CLOCK: timeDisplay_HMS();break;
 				case Y_M_D: timeDisplay_YMD();break;
+				case TEMPTURE: temptureDisplay();break;
 			}
 			cnt = 0;
 			writeOpen(1);
 		}
-		statesAlter();
+		statesAlter(); //判断按钮按下 状态切换	
 	}
 }
